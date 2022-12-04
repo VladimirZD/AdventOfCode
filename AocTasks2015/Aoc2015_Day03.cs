4cf498cfa486c17c6a9c96b89522532def6558fa
Vladimir Mašala <vm@snowfalltravel.com>
2022-12-04 15:33:59 +0100

    Day 4

--- AocTasks2015/Aoc2015_Day03.cs
+++ AocTasks2015/Aoc2015_Day03.cs
@@ -1,67 +1,72 @@
-﻿//using AdventOfCode.Attributes;
-//using AdventOfCode.Interfaces;
-//using System.Runtime.InteropServices;
-//using static AdventOfCode.AocTasks2021.Aoc2021_Day09;
-//using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using AdventOfCode.Attributes;
+using AdventOfCode.Interfaces;
+using System.IO;
+using System.Runtime.InteropServices;
+using static AdventOfCode.AocTasks2021.Aoc2021_Day09;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-//namespace AdventOfCode.AocTasks2015
-//{
-//    [AocTask(2015, 3)]
-//    public class Aoc2015_Day03 : IAocTask
-//    {
-//        public List<List<int>> PackageData { get; set; }
-//        public Aoc2015_Day03(string filePath)
-//        {
-//            PackageData = LoadTaskinput(filePath);
-//        }
-//        static List<List<int>> LoadTaskinput(string filePath)
-//        {
-//            var data = System.IO.File.ReadAllLines(filePath).Select(l => l.Split('x').Select(int.Parse).ToList()).ToList();
-//            return data;
-//        }
+namespace AdventOfCode.AocTasks2015
+{
+    [AocTask(2015, 3)]
+    public class Aoc2015_Day03 : IAocTask
+    {
+        public string Directions{ get; set; }
+        public Aoc2015_Day03(string filePath)
+        {
+            Directions= LoadTaskinput(filePath);
+        }
+        static string LoadTaskinput(string filePath)
+        {
+            var input = System.IO.File.ReadAllText(filePath);
+            var data = input;
+            return data;
+        }
+        
+        string IAocTask.Solve1()
+        {
+            HashSet<(int, int)> houses = VisitHouses(Directions);
+            return houses.Count.ToString();
+        }
 
-//        public static int GetArea(int l, int w, int h)
-//        {
-//            /*2*l*w + 2*w*h + 2*h*l*/
-//            return 2 * (l * w + w * h + h * l);
-//        }
-//        string IAocTask.Solve1()
-//        {
-//            var totalArea = 0L;
-//            foreach (var package in PackageData)
-//            {
-//                var area = GetArea(package[0], package[1], package[2]);
-//                var sides = package.OrderBy(p => p).Take(2).ToList();
-//                var extra = sides[0] * sides[1];
-//                //var ribbon =
-//                totalArea += (area + extra);
-//            }
-//            return totalArea.ToString();
-//        }
-//        string IAocTask.Solve2()
-//        {
+        static private HashSet<(int, int)> VisitHouses(string directions)
+        {
+            int x = 0;
+            int y = 0;
+            var houses = new HashSet<(int, int)> { (x, y) };
 
-//            /*
-//             *The elves are also running low on ribbon. Ribbon is all the same width, so they only have to worry about the length they need to order, which they would again like to be exact.
-//                The ribbon required to wrap a present is the shortest distance around its sides, or the smallest perimeter of any one face. Each present also requires a bow made out of ribbon as well; 
-//                the feet of ribbon required for the perfect bow is equal to the cubic feet of volume of the present. Don't ask how they tie the bow, though; they'll never tell.
-//                For example:
-//                A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
-//                A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet.
-//                How many total feet of ribbon should they order?
-//             * */
-//            var totalRibon = 0L;
-//            foreach (var package in PackageData)
-//            {
-//                var sides = package.OrderBy(p => p).Take(2).ToList();
-//                var ribbon = 2 * (sides[0] + sides[1]);
-//                var bow = package[0] * package[1] * package[2];
-//                totalRibon += (ribbon + bow);
-//            }
-//            return totalRibon.ToString();
-//        }
-//    }
-//}
+            for (var i = 0; i < directions.Length; i++)
+            {
+                var direction = directions[i];
+                switch (direction)
+                {
+                    case '>':
+                        x++;
+                        break;
+                    case '<':
+                        x--;
+                        break;
+                    case '^':
+                        y++;
+                        break;
+                    case 'v':
+                        y--;
+                        break;
+                }
+                houses.Add((x, y));
+            }
+
+            return houses;
+        }
+
+
+        string IAocTask.Solve2()
+        {
+            HashSet<(int, int)> housesSanta = VisitHouses(new string(Directions.Where((c, i) => i % 2 !=0).ToArray()));
+            HashSet<(int, int)> housesRobo = VisitHouses(new string(Directions.Where((c, i) => i % 2 == 0).ToArray()));
+            return (housesSanta.Union(housesRobo)).Count().ToString();
+        }
+    }
+}
 
 
 
--- Attributes/AocTaskAttribute.cs
+++ Attributes/AocTaskAttribute.cs
@@ -5,10 +5,18 @@
     {
         public int Year { get; set; }
         public int Day { get; set; }
+        public string? Description { get; set; }
         public AocTask(int year, int day)
         {
             Year = year;
             Day = day;
+        }
+
+        public AocTask(int year, int day,string descirption)
+        {
+            Year = year;
+            Day = day;
+            Description = descirption;
         }
     }
 }
