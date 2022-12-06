79aadc1cca63b9826e2794905ae379f8b32d9fc3
Vladimir Mašala <vm@snowfalltravel.com>
2022-12-06 09:55:44 +0100

    Cleanup

---
+++ AocTasks2022/Day_1_Calorie_Counting.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCode.Attributes;
+using AdventOfCode.Interfaces;
+
+namespace AdventOfCode.AocTasks2022
+{
+    [AocTask(2022, 1)]
+    public class Day_1_Calorie_Counting : IAocTask
+    {
+        public string FilePath { get; set; }
+        public List<int> Numbers { get; set; }
+        public Day_1_Calorie_Counting(string filePath)
+        {
+            FilePath= filePath;
+        }
+        public void PrepareData()
+        {
+            Numbers = System.IO.File.ReadAllText(FilePath).Split("\n\n").Select(l => l.Split("\n").Where(i => !string.IsNullOrEmpty(i)).Select(int.Parse).Sum(i => i)).ToList();
+        }
+        string IAocTask.Solve1()
+        {
+            return Numbers.Max().ToString();
+        }
+        string IAocTask.Solve2()
+        {
+            return Numbers.OrderByDescending(i => i).Take(3).Sum().ToString();
+        }
+    }
+}
+
+
+
+
+
