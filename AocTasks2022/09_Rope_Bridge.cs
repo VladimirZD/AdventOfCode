48d4561a275f620666382636847205eefaf9dac2
Vladimir Mašala <vm@snowfalltravel.com>
2022-12-10 17:47:05 +0100

    Day 9 & 10

---
+++ AocTasks2022/09_Rope_Bridge.cs
@@ -0,0 +1,116 @@
+﻿using AdventOfCode.Attributes;
+using AdventOfCode.Interfaces;
+using Newtonsoft.Json.Linq;
+using Org.BouncyCastle.X509;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+
+namespace AdventOfCode.AocTasks2022
+{
+    [AocTask(2022, 9)]
+    public class Rope_Bridge : IAocTask
+    {
+
+        private string[] Input { get; set; }
+        private string[] Instructions { get; set; }
+        private Cell[] Knots { get; set; }
+        private int Solution2 { get; set; }
+        public HashSet<string> VisitedPositions1 { get; set; }
+        public HashSet<string> VisitedPositions10 { get; set; }
+
+        record struct Cell(int X, int Y);
+
+        public Rope_Bridge(string filePath)
+        {
+            Input = File.ReadAllLines(filePath);
+            //Input = "R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2".Split("\n").ToArray();
+            //Input = "R 5\r\nU 8\r\nL 8\r\nD 3\r\nR 17\r\nD 10\r\nL 25\r\nU 20".Split("\r\n").ToArray();
+        }
+        public void PrepareData()
+        {
+            VisitedPositions1 = new HashSet<string>();
+            VisitedPositions10 = new HashSet<string>();
+            Instructions = Input;
+            Knots = Enumerable.Repeat(new Cell(0, 0), 10).ToArray();
+        }
+        string IAocTask.Solve1()
+        {
+            DoTheWalk(Knots);
+            var retValue = VisitedPositions1.Count();
+            Debug.Assert(retValue == 6044);
+            return retValue.ToString();
+        }
+
+        private void DoTheWalk(Cell[] tails)
+        {
+            
+            VisitedPositions1.Add($"{Knots[1].X}:{Knots[1].Y}");
+            VisitedPositions10.Add($"{Knots[1].X}:{Knots[1].Y}");
+            
+            var span = Instructions.AsSpan();
+            for(var i=0;i<span.Length;i++)
+            {
+                var direction = span[i][0];
+                var distance = int.Parse(span[i][2..]);
+                int dx=0;
+                int dy=0;
+
+                switch (direction) 
+                {
+                    case 'U':
+                        dy = 1;
+                        break;
+                    case 'D':
+                        dy = -1;
+                        break;
+                    case 'L':
+                        dx = -1;
+                        break;
+                    case 'R':
+                        dx = 1;
+                        break;
+                }
+                for (var d = 0; d < distance; d++)
+                {
+                    Knots[0].X = Knots[0].X + dx;
+                    Knots[0].Y = Knots[0].Y + dy;
+                    for (var j= 1;j < Knots.Length;j++)
+                    {
+                        var distX = Knots[j-1].X - Knots[j].X;
+                        var distY = Knots[j-1].Y - Knots[j].Y;
+                        if (Math.Abs(distX) > 1 || Math.Abs(distY) > 1)
+                        {
+                            Knots[j].X += Math.Sign(distX);
+                            Knots[j].Y += Math.Sign(distY);
+                            if (j == 1)
+                            {
+                                VisitedPositions1.Add($"{Knots[j].X}:{Knots[j].Y}");
+                            }
+                            if (j==9)
+                            {
+                                VisitedPositions10.Add($"{Knots[j].X}:{Knots[j].Y}");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private static double GetDistance(Cell start,Cell end)
+        {
+            var retValue = Math.Pow(end.X - start.X, 2);
+            retValue += Math.Pow(end.Y - start.Y, 2);
+            return retValue;
+        }
+
+        string IAocTask.Solve2()
+        {
+            var retValue = VisitedPositions10.Count();
+            Debug.Assert(retValue == 2384);
+            return retValue.ToString();
+        }
+
+    }
+}
+
---
+++ AocTasks2022/10_Cathode-Ray_Tube .cs
@@ -0,0 +1,81 @@
+﻿using AdventOfCode.Attributes;
+using AdventOfCode.Interfaces;
+using Newtonsoft.Json.Linq;
+using Org.BouncyCastle.X509;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+
+namespace AdventOfCode.AocTasks2022
+{
+    [AocTask(2022, 10)]
+    public class Cathod_Ray_Tube : IAocTask
+    {
+        const int WIDTH = 40;
+        const int HEIGHT = 6;
+
+        private string[] Input { get; set; }
+        private int Result1 { get; set; }
+        private Char[,] Crt { get; set; }
+
+        public Cathod_Ray_Tube(string filePath)
+        {
+            Input = File.ReadAllLines(filePath).Select(i => i.Split(' ')).SelectMany(i => i).ToArray(); ; ;
+            //Input = "addx 15\r\naddx -11\r\naddx 6\r\naddx -3\r\naddx 5\r\naddx -1\r\naddx -8\r\naddx 13\r\naddx 4\r\nnoop\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx -35\r\naddx 1\r\naddx 24\r\naddx -19\r\naddx 1\r\naddx 16\r\naddx -11\r\nnoop\r\nnoop\r\naddx 21\r\naddx -15\r\nnoop\r\nnoop\r\naddx -3\r\naddx 9\r\naddx 1\r\naddx -3\r\naddx 8\r\naddx 1\r\naddx 5\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\naddx -36\r\nnoop\r\naddx 1\r\naddx 7\r\nnoop\r\nnoop\r\nnoop\r\naddx 2\r\naddx 6\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\naddx 1\r\nnoop\r\nnoop\r\naddx 7\r\naddx 1\r\nnoop\r\naddx -13\r\naddx 13\r\naddx 7\r\nnoop\r\naddx 1\r\naddx -33\r\nnoop\r\nnoop\r\nnoop\r\naddx 2\r\nnoop\r\nnoop\r\nnoop\r\naddx 8\r\nnoop\r\naddx -1\r\naddx 2\r\naddx 1\r\nnoop\r\naddx 17\r\naddx -9\r\naddx 1\r\naddx 1\r\naddx -3\r\naddx 11\r\nnoop\r\nnoop\r\naddx 1\r\nnoop\r\naddx 1\r\nnoop\r\nnoop\r\naddx -13\r\naddx -19\r\naddx 1\r\naddx 3\r\naddx 26\r\naddx -30\r\naddx 12\r\naddx -1\r\naddx 3\r\naddx 1\r\nnoop\r\nnoop\r\nnoop\r\naddx -9\r\naddx 18\r\naddx 1\r\naddx 2\r\nnoop\r\nnoop\r\naddx 9\r\nnoop\r\nnoop\r\nnoop\r\naddx -1\r\naddx 2\r\naddx -37\r\naddx 1\r\naddx 3\r\nnoop\r\naddx 15\r\naddx -21\r\naddx 22\r\naddx -6\r\naddx 1\r\nnoop\r\naddx 2\r\naddx 1\r\nnoop\r\naddx -10\r\nnoop\r\nnoop\r\naddx 20\r\naddx 1\r\naddx 2\r\naddx 2\r\naddx -6\r\naddx -11\r\nnoop\r\nnoop\r\nnoop".Split("\r\n").Select(i => i.Split(' ')).SelectMany(i => i).ToArray(); ;
+            //Input= "noop\r\naddx 3\r\naddx -5".Split("\r\n").Select(i=>i.Split(' ')).SelectMany(i=>i).ToArray();
+        }
+        public void PrepareData()
+        {
+            Crt = new char[HEIGHT, WIDTH];
+            var x = 1;
+            double totalValue = 0;
+
+            var instructions = Input.AsSpan();
+            for (var i = 0; i < instructions.Length; i++)
+            {
+                double cycle = i + 1;
+                int currentRow = (int)((i / WIDTH) % HEIGHT);
+                int currentCol = (int)(i % WIDTH);
+                if ((x == currentCol || (x - 1) == currentCol) || (x + 1) == currentCol)
+                {
+                    Crt[currentRow, currentCol] = '#';
+                }
+                var cmd = instructions[i];
+                if (cycle == 20 || (cycle + 20) % 40 == 0)
+                {
+                    totalValue += cycle * x;
+                }
+                if (int.TryParse(cmd, out int value))
+                {
+                    x += value;
+                }
+            }
+            Result1 = (int)totalValue;
+        }
+        string IAocTask.Solve1()
+        {
+            return Result1.ToString();
+        }
+        string IAocTask.Solve2()
+        {
+            var retValue = GetPrintData(Crt);
+            return retValue.ToString();
+        }
+        private string GetPrintData(char[,] data)
+        {
+            var line = Environment.NewLine;
+            for (var y = 0; y < HEIGHT; y++)
+            {
+                for (var x = 0; x < WIDTH; x++)
+                {
+                    line += data[y, x] == '#' ? '#' : '.';
+                }
+                line += Environment.NewLine;
+            }
+            return line;
+        }
+
+    }
+}
+
