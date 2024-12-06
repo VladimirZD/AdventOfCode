c6ce406ce83192b9d5afe991d1e1706eecdfbb95
Vladimir Mašala <vm@snowfalltravel.com>
2024-12-06 20:04:35 +0100

    Day 4&5 done

---
+++ AocTasks2024/04_Ceres_Search.cs
@@ -0,0 +1,107 @@
+﻿using AdventOfCode.Attributes;
+using AdventOfCode.Interfaces;
+using System.Diagnostics;
+
+namespace AdventOfCode.AocTasks2024
+{
+    [AocTask(2024, 4)]
+    public class Ceres_Search(string filePath) : IAocTask
+    {
+        public string FilePath { get; set; } = filePath;
+        public long Sol1 { get; set; }
+        public long Sol2 { get; set; }
+        public char[,] TaskData { get; set; }
+        public List<(int, int)> StartingXCells = new List<(int, int)>();
+        public List<(int, int)> StartingACells = new List<(int, int)>();
+
+        private int[] dx = { -1, -1, -1, 0, 1, 1, 1, 0 }; 
+        private int[] dy = { -1, 0, 1, 1, 1, 0, -1, -1 }; 
+
+        public void PrepareData()
+        {
+
+            var inputLines = File.ReadAllText(filePath);
+            ///inputLines = "MMMSXXMASM\r\nMSAMXMSMSA\r\nAMXSXMAAMM\r\nMSAMASMSMX\r\nXMASAMXAMM\r\nXXAMMXXAMA\r\nSMSMSASXSS\r\nSAXAMASAAA\r\nMAMMMXMMMM\r\nMXMXAXMASX".Replace("\r\n", "\n");
+
+            var rows = inputLines.Split('\n');
+            TaskData = new char[rows.Length, rows[0].Length];
+            for (int i = 0; i < rows.Length; i++)
+            {
+                for (int j = 0; j < rows[i].Length; j++)
+                {
+                    TaskData[i, j] = rows[i][j];
+                    if (rows[i][j] == 'X')
+                    {
+                        StartingXCells.Add((i, j));
+                    }
+                }
+            }
+            Sol1 = 0;
+            Sol2 = 0;
+        }
+        string IAocTask.Solve1()
+        {
+            string pattern= "XMAS";
+            foreach (var cell in StartingXCells)
+            {
+                Sol1+=WalkTheMap(cell.Item1, cell.Item2, pattern);
+            }
+            Debug.Assert(Sol1 == 18 || Sol1 == 2493);
+            return Sol1.ToString();
+        }
+        string IAocTask.Solve2()
+        {
+            for (int row = 1; row < TaskData.GetLength(0) - 1; row++)
+            {
+                for (int col = 1; col < TaskData.GetLength(1) - 1; col++)
+                {
+                    if (IsXShape(row,col))
+                    {
+                            Sol2++;
+                    }
+                }
+            }
+            Debug.Assert(Sol2 == 1890 || Sol2 == 48);
+            return Sol2.ToString();
+        }
+
+        private bool IsXShape(int row, int col)
+        {
+
+            if (row - 1 < 0 || col - 1 < 0 || row + 1 >= TaskData.GetLength(0) || col + 1 >= TaskData.GetLength(1))
+                return false;
+            if (TaskData[row, col] != 'A') return false;
+
+            bool isTopLeftBottomRightValid =
+                (TaskData[row - 1, col - 1] == 'M' && TaskData[row + 1, col + 1] == 'S') ||
+                (TaskData[row - 1, col - 1] == 'S' && TaskData[row + 1, col + 1] == 'M');
+
+            bool isTopRightBottomLeftValid =
+                (TaskData[row - 1, col + 1] == 'S' && TaskData[row + 1, col - 1] == 'M') ||
+                (TaskData[row - 1, col + 1] == 'M' && TaskData[row + 1, col - 1] == 'S');
+
+            return isTopLeftBottomRightValid && isTopRightBottomLeftValid;
+        }
+
+        bool IsPatternFound(int row, int col, int index, int direction,string pattern)
+        {
+            if (index == pattern.Length) return true;
+            if (row < 0 || row >= TaskData.GetLength(0) || col < 0 || col >= TaskData.GetLength(1)) return false;
+            if (TaskData[row, col] != pattern[index]) return false;
+
+            return IsPatternFound(row + dx[direction], col + dy[direction], index + 1, direction, pattern);
+        }
+        private int WalkTheMap(int x, int y,string patern)
+        {
+            var retValue = 0;
+            for (var i = 0; i < 8; i++)
+            {
+                if (IsPatternFound(x, y, 0, i,patern))
+                {
+                    retValue++;
+                }
+            }
+            return retValue;
+        } 
+    }
+}
---
+++ AocTasks2024/05_Print_Queue.cs
@@ -0,0 +1,85 @@
+﻿using AdventOfCode.Attributes;
+using AdventOfCode.Interfaces;
+using BenchmarkDotNet.Validators;
+using Microsoft.Diagnostics.Tracing.Parsers.AspNet;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace AdventOfCode.AocTasks2024
+{
+    [AocTask(2024, 5)]
+    public class Print_Queue(string filePath) : IAocTask
+    {
+        public string FilePath { get; set; } = filePath;
+        public long Sol1 { get; set; }
+        public long Sol2 { get; set; }
+        private Dictionary<int,List<int>> OrderingRules = new Dictionary<int, List<int>>();
+        private List<int[]> Updates= new List<int[]>();
+
+        public void PrepareData()
+        {
+            var inputLines = File.ReadAllText(filePath);
+            //inputLines = "47|53\r\n97|13\r\n97|61\r\n97|47\r\n75|29\r\n61|13\r\n75|53\r\n29|13\r\n97|29\r\n53|29\r\n61|53\r\n97|53\r\n61|29\r\n47|13\r\n75|47\r\n97|75\r\n47|61\r\n75|61\r\n47|29\r\n75|13\r\n53|13\r\n\r\n75,47,61,53,29\r\n97,61,53,29,13\r\n75,29,13\r\n75,97,47,61,53\r\n61,13,29\r\n97,13,75,29,47".Replace("\r\n", "\n");
+            var inputData = inputLines.Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
+            var rules = inputData[0].Split("\n", StringSplitOptions.RemoveEmptyEntries);
+            Updates = inputData[1].Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(line => line.Split(',').Select(int.Parse).ToArray()).ToList();
+
+            foreach (var  item in rules)
+            {
+                var pages=item.Split("|");
+                var key=int.Parse(pages[1]);
+                if (OrderingRules.ContainsKey(key))
+                {
+                    OrderingRules[key].Add(int.Parse(pages[0]));
+                }
+                else
+                {
+                    OrderingRules.Add(key, new List<int>() {int.Parse(pages[0]) });
+                }
+            }
+            Sol1 = 0;
+            Sol2 = 0;
+        }
+        string IAocTask.Solve1()
+        {
+            foreach (var update in Updates)
+            {
+                var index = update.Length / 2;
+                if (OrderIt(update))
+                {
+                    Sol1 += update[index];
+                }
+                else
+                {
+                    Sol2 += update[index];
+                }
+            }
+            Debug.Assert(Sol1 == 143 || Sol1 == 5762);
+            return Sol1.ToString();
+        }
+        private bool OrderIt(int[] pages)
+        {
+            var retValue = true;
+            for (var i = 0; i < pages.Length-1; i++)
+            {
+                for (var j=i+1;j<pages.Length; j++)
+                {
+                    if (OrderingRules.TryGetValue(pages[i], out var rule))
+                    {
+                        if (rule.Contains(pages[j]))
+                        {
+                            (pages[i], pages[j]) = (pages[j], pages[i]);
+                            retValue = false;
+                        }
+                    }
+                }
+            }
+            return retValue;
+        }
+        string IAocTask.Solve2()
+        {
+            Debug.Assert(Sol2 == 123 || Sol2 == 4130);
+            return Sol2.ToString();
+        }
+    }
+}
